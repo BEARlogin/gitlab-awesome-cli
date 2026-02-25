f313d6ca45846dc3ec408c583c85e4219a91f4b2
Andrey Lugovskoy <bearlogin@mail.ru>
2026-02-25 16:30:00 +0400

    feat: initial project scaffolding

---
+++ cmd/glcli/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("glcli - GitLab Awesome CLI")
+	os.Exit(0)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bearlogin/gitlab-awesome-cli
+
+go 1.25.0
